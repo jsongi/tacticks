e6dd15ec552af1f65c77e900eda325eebee26a0c
Jason G <42660969+jsongi@users.noreply.github.com>
2025-03-04 13:22:59 -0800

    updating jack in patrons

--- shop.py
+++ shop.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import copy
 
 from unit import Unit
 from patron import Patron
@@ -26,27 +27,28 @@ class Shop:
         ]
 
         self._patrons = [
-            Patron(23, 3, 20, 4, "Plague Doctor"),
+            Patron(23, 3, 20, 6, "Plague Doctor"),
+            Patron(24, 5, 14, 4, "Jack"),
             Patron(23, 3, 20, 4, "Placeholder"),
-            Patron(23, 3, 20, 4, "Placeholder"),
+            Patron(23, 3, 20, 4, "Placeholder")
         ]
 
         self._units = [
-            Unit(40, 3, 1, 2, 0, "Knight", 4),
-            Unit(30, 2, 1, 4, 0, "Thief", 4),
-            Unit(25, 4, 3, 3, 0, "Archer", 4),
-            Unit(20, 6, 3, 2, 0, "Wizard", 4),
-            Unit(20, 2, 3, 3, 0, "Healer", 3),
-            Unit(70, 15, 1, 2, 0, "Executioner", 8),
-            Unit(50, 10, 1, 4, 0, "Gambler", 8),
-            Unit(40, 40, 20, 1, 0, "Ballista", 8),
-            Unit(40, 25, 4, 2, 0, "Archmage", 8),
-            Unit(30, 20, 4, 2, 0, "Mystic", 8)
+            Unit(40, 3, 0, 1, 2, 0, "Knight", 4),
+            Unit(30, 2, 0, 1, 4, 0, "Thief", 4),
+            Unit(25, 4, 0, 3, 3, 0, "Archer", 4),
+            Unit(20, 0, 6, 3, 2, 0, "Wizard", 4),
+            Unit(20, 0, 3, 3, 3, 0, "Healer", 3),
+            Unit(70, 15, 0, 1, 2, 0, "Executioner", 8),
+            Unit(50, 10, 0, 1, 4, 0, "Gambler", 8),
+            Unit(40, 40, 0, 20, 1, 0, "Ballista", 8),
+            Unit(40, 0, 25, 4, 2, 0, "Archmage", 8),
+            Unit(30, 0, 20, 4, 2, 0, "Mystic", 8)
         ]
         # TODO: The images slots for this are temporary and need to be updated
         self._upgrades = [
             Upgrade(22, 1, 1, 4, "Flat HP"),
-            Upgrade(22, 1, 2, 4, "Scaling HP"),
+            Upgrade(22, 1, 2, 4, "Flat Magic"),
             Upgrade(22, 1, 3, 4, "Flat Attack")
         ]
 
@@ -69,11 +71,12 @@ class Shop:
 
         # TODO: Should be weighted differently by rarity
         self.displayed_patrons = random.sample(
-            [p for p in self._patrons if p not in self._owned_patrons], 3
+            [p for p in self._patrons if p not in self._owned_patrons and not p.getPurchased()], 3
         )
         self.displayed_units = random.sample(
             [u for u in self._units], 2
         )
+        # Potentially make this not rerollable
         self.displayed_upgrades = random.sample(
             [u for u in self._upgrades], 2
         )
@@ -85,28 +88,31 @@ class Shop:
 
         y_offset = 150  # Start Y position for patrons
         for i, item in enumerate(self.displayed_patrons):
-            pygame.draw.rect(screen, (0, 200, 0), (300, y_offset, 200, 50))
-            text_surface = font.render(f"{item.getName()} - ${item.getCost()}", True, (0, 0, 0))
-            screen.blit(text_surface, (310, y_offset + 10))
-            screen.blit(self._images[item.getImage()], (430, y_offset - 22))
+            if item:
+                pygame.draw.rect(screen, (0, 200, 0), (300, y_offset, 200, 50))
+                text_surface = font.render(f"{item.getName()} - ${item.getCost()}", True, (0, 0, 0))
+                screen.blit(text_surface, (310, y_offset + 10))
+                screen.blit(self._images[item.getImage()], (430, y_offset - 22))
             y_offset += 70
 
         # Flat upgrades (Left Side)
         y_offset = 200
         for i, item in enumerate(self.displayed_upgrades):
-            pygame.draw.rect(screen, (200, 0, 200), (50, y_offset, 200, 50))
-            text_surface = font.render(f"{item.getName()} - ${item.getCost()}", True, (0, 0, 0))
-            screen.blit(text_surface, (60, y_offset + 10))
-            screen.blit(self._images[item.getImage()], (180, y_offset - 22))
+            if item:
+                pygame.draw.rect(screen, (200, 0, 200), (50, y_offset, 200, 50))
+                text_surface = font.render(f"{item.getName()} - ${item.getCost()}", True, (0, 0, 0))
+                screen.blit(text_surface, (60, y_offset + 10))
+                screen.blit(self._images[item.getImage()], (180, y_offset - 22))
             y_offset += 70
 
         # Units (Right Side)
         y_offset = 200
         for i, item in enumerate(self.displayed_units):
-            pygame.draw.rect(screen, (0, 0, 200), (550, y_offset, 200, 50))
-            text_surface = font.render(f"{item.type()} - ${item.getCost()}", True, (255, 255, 255))
-            screen.blit(text_surface, (560, y_offset + 10))
-            screen.blit(self._images[item.typeImage()], (680, y_offset - 22))
+            if item:
+                pygame.draw.rect(screen, (0, 0, 200), (550, y_offset, 200, 50))
+                text_surface = font.render(f"{item.type()} - ${item.getCost()}", True, (255, 255, 255))
+                screen.blit(text_surface, (560, y_offset + 10))
+                screen.blit(self._images[item.typeImage()], (680, y_offset - 22))
             y_offset += 70
 
         # Reroll Button
@@ -119,9 +125,14 @@ class Shop:
         continue_text = font.render(f"Continue", True, (0, 0, 0))
         screen.blit(continue_text, (510, 515))
 
+        # Gold Value
+        pygame.draw.rect(screen, (255, 215, 0), (350, 600, 100, 50))
+        gold_text = font.render(f"Gold: {gold}", True, (0, 0, 0))
+        screen.blit(gold_text, (360, 615))
+
         pygame.display.flip()
 
-    def check_click(self, mouse_x, mouse_y, cell_width, cell_height):
+    def check_click(self, mouse_x, mouse_y):
         """ Determines if an item or the reroll button was clicked. """
         # Check patrons
         y_offset = 150
@@ -150,11 +161,15 @@ class Shop:
 
         # Check continue button
         if 500 <= mouse_x <= 600 and 500 <= mouse_y <= 550:
+            self.reroll_cost = 5
             return "continue"
 
         return None
 
-    def handle_click(self, gold, clicked_asset, patrons, chars, upgrades):
+    def apply_upgrades(self, char, upgrade):
+        return
+
+    def handle_click(self, gold, clicked_asset, patrons, chars):
         """ Handles purchases and rerolling. """
 
         result = 1
@@ -162,6 +177,7 @@ class Shop:
             return gold, result
         if clicked_asset == "reroll" and gold >= self.reroll_cost:
             gold -= self.reroll_cost
+            self.reroll_cost += 2
             self.refresh_items()
             return gold, result
         elif clicked_asset == "continue":
@@ -169,20 +185,23 @@ class Shop:
             return gold, result  # Return updated gold amount
         elif clicked_asset in self._patrons and gold >= clicked_asset.getCost():
             gold -= clicked_asset.getCost()
-            # TODO: Mark item as purchased
+            clicked_asset.setPurchased(True)
             patrons.append(clicked_asset)
-            # TODO: Remove bought items from display
+            self.displayed_patrons[self.displayed_patrons.index(clicked_asset)] = None
             return gold, result
-        # Calculate new id for unit in board display
         elif clicked_asset in self._units and gold >= clicked_asset.getCost() and len(chars) < 6:
             gold -= clicked_asset.getCost()
+            # TODO: Calculate new id for unit in board display when characters are sold and new ones are bought
             clicked_asset.setId(len(chars) + 4)
-            chars.append(clicked_asset)
-            print(chars)
+            chars.append(copy.copy(clicked_asset))
+            self.displayed_units[self.displayed_units.index(clicked_asset)] = None
             return gold, result
         elif clicked_asset in self._upgrades and gold >= clicked_asset.getCost():
             gold -= clicked_asset.getCost()
-            upgrades.append(clicked_asset)
+            for char in chars:
+                self.apply_upgrades(char, clicked_asset)
+            self.displayed_patrons[self.displayed_patrons.index(clicked_asset)] = None
             return gold, result
         else:
             return gold, result
+        
