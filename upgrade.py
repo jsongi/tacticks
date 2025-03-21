977e5a8883a4dc6ddc222371f75dc56f039c0330
Jason G <42660969+jsongi@users.noreply.github.com>
2025-03-20 21:40:14 -0700

    reworking upgrades to be permanent

--- upgrade.py
+++ upgrade.py
@@ -1,18 +1,17 @@
 class Upgrade:
 
-    def __init__(self, imageNum, effectType, effectIndex, cost, name):
+    def __init__(self, imageNum, effectIndex, cost, name):
         self._imageNum = imageNum
-        self.effectType = effectType
         self.effectIndex = effectIndex
         self._cost = cost
         self._name = name
+        self._totalHealth = 0
+        self._magic = 0
+        self._attack = 0
+        self._healing = 0
 
     def getImage(self):
         return self._imageNum
-    
-    # Immediate (1), all are immediate for right now, maybe unlocks for others in the future
-    def getEffectType(self):
-        return self.effectType
     
     def getCost(self):
         return self._cost
@@ -26,14 +25,35 @@ class Upgrade:
             case 1:
                 for char in chars:
                     char.addTotalHealth(15)
+                self._totalHealth += 15
                 return
             case 2:
                 for char in chars:
                     char.addMagic(5)
+                self._magic += 5
                 return
             case 3:
                 for char in chars:
                     char.addAttack(5)
+                self._attack += 5
                 return
-        
+            case 4:
+                self._healing += 2
+                return
+        return
+    
+        # Handles effects
+    def onUnitPurchase(self, char):
+        match self.effectIndex:
+            case 1:
+                char.addTotalHealth(15)
+                return
+            case 2:
+                char.addMagic(5)
+                return
+            case 3:
+                char.addAttack(self._attack)
+                return
+            case 4:
+                return
         return
